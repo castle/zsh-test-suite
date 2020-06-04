de6bdc1f8aa93ee7e5fa246ca62739de91d359a9
Aron Eidelman <aron@eideldev.com>
2020-06-03 22:34:34 -0700

    lower attack probablity

--- random_traffic.zsh
+++ random_traffic.zsh
@@ -2,17 +2,17 @@
 
 while true; do
   DICE=$((RANDOM %= 100))
-  if [[ $DICE < 40 ]]; then
+  if [[ $DICE < 60 ]]; then
     zsh ./login_from_new_client.sh;
-  elif [[ $DICE < 70 ]]; then
-    zsh ./login_from_new_region.sh;
   elif [[ $DICE < 90 ]]; then
+    zsh ./login_from_new_region.sh;
+  elif [[ $DICE < 94 ]]; then
     zsh ./forgetful_user.sh;
-  elif [[ $DICE < 93 ]]; then
-    zsh ./credential_stuffing.sh;
   elif [[ $DICE < 96 ]]; then
+    zsh ./credential_stuffing.sh;
+  elif [[ $DICE < 98 ]]; then
     zsh ./brute_force.sh;
-  elif [[ $DICE > 101 ]]; then
+  elif [[ $DICE < 101 ]]; then
     zsh ./brute_force_botnet.sh;
   fi
   date ;
